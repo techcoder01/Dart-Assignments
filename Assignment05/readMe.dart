fb62470b73600c402dbf1a9956331fd391db64d7
Abdul Hannan <121096664+techcoder01@users.noreply.github.com>
2023-03-10 23:42:30 +0500

    Create readMe.dart

---
+++ Assignment05/readMe.dart
@@ -0,0 +1,18 @@
+Q1:  Map<String, double> expenses = {
+  'sun': 3000.0,
+  'mon': 3000.0,
+  'tue': 3234.0,
+};
+Check if "fri" exist in expanses; if exist change it's value to 5000.0 otherwise add 'fri' to expenses and set its value to 5000.0 then print expenses.
+
+Q2: Write a program in Dart that find the area of a circle using function.
+
+Q3: Write a program in a dart that implements the Pythagorean theorem using function.
+
+Q4: Write a program in Dart to reverse a String using function.
+
+Q5: Write a program in Dart to signup user by using username, email & password as required fields while  phoneNumber is optional using function, call this function at least 3 times to create different users. Now create another function named as loginUser and then try to login with correct & incorrect credentials
+
+Q6:  Write a program in Dart to calculate power of a certain numbers using function only.
+
+Q7: Write a function to calculate number of vowels and consonant in a String.
